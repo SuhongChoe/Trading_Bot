d109fa190a60401db1322e80eedd737d4a943737
choesuhong <csoohong64@gmail.com>
2021-10-14 05:16:11 +0900

    install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3-pip
+pip3 install pyupbit
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+sudo apt install python3-pip
+pip3 install schedule
+pip3 install pystan==2.19.1.1
+pip3 install convertdate
+pip3 install fbprophet
